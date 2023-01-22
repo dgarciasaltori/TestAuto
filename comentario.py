4afdc216a426bff48b6fa93b3d27fff5dfb90c8a
Diego <dgarcia.saltori@me.com>
2023-01-22 13:06:06 -0300

    Update20230122

--- comentario.py
+++ comentario.py
@@ -1,6 +1,8 @@
 #Code por Diego Saltori
+#Language - PT-BR, EN-US
 #UTF-8
 #Para rodar o código: pytest --html=report.html comentario.py
+#To run the code: pytest --html=report.html comentario.py
 
 import string, time, random, logging
 from faker import Faker
@@ -8,21 +10,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 #Criando textos aleátrios
-
+#Creating random text
 fake = Faker()
 random_text = fake.text(250)
 first_name = fake.first_name()
 last_name = fake.last_name()
 
 # Gerando uma string aleatória com 8 caracteres
+# Generating a random string with 8 characters
 random_string = ''.join(random.choices(string.ascii_letters + string.digits, k=8))
 
 # Adicionando a string aleatória ao nome do arquivo
+# Adding the random string to the filename
 screenshot_name = "screenshot_" + random_string + ".png"
 
 #Criando Log de Eventos 
+#Creating Event Log
 logging.basicConfig(filename='test_log.log', level=logging.INFO)
 
+#Função para iniciar o Chrome, abrir o site desejado e criar o loop de inserir comentários e finalizando com o printscreen
+#Function to start Chrome, open the desired site and create the loop of inserting comments and ending with the printscreen
 def test_add_comments():
     driver = webdriver.Chrome()
     driver.set_window_size(800, 600)
@@ -38,7 +45,8 @@ def test_add_comments():
     assert "Comentários: 10" in driver.page_source
     driver.save_screenshot(screenshot_name)
 
-#Fechando o browser
+#Fechando o Chrome
+#Closing Chrome
 def closer():
     driver = webdriver.Chrome()
     driver.close() #ou quit()
